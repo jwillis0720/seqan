c57b56423737d299d4c53713f9ee2539b66b8315
Jordan Willis <jordanwillis@dhcp-231-56.scripps.edu>
2015-01-26 14:40:04 -0800

    Adding working build

---
+++ src/Default/subdir.mk
@@ -0,0 +1,36 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../AlignAntibody.cpp \
+../AntibodyJunction.cpp \
+../DatabaseHandler.cpp \
+../SeqIg.cpp \
+../SeqIgUtility.cpp 
+
+OBJS += \
+./AlignAntibody.o \
+./AntibodyJunction.o \
+./DatabaseHandler.o \
+./SeqIg.o \
+./SeqIgUtility.o 
+
+CPP_DEPS += \
+./AlignAntibody.d \
+./AntibodyJunction.d \
+./DatabaseHandler.d \
+./SeqIg.d \
+./SeqIgUtility.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I/Users/jordanwillis/Git_repos/seqig_standalone/include -O2 -g -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
